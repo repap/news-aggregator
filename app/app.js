9e63fbaeac3d640d2e64e2f3e30bbce39915ae46
Dennis Penning <penning.dennis@googlemail.com>
2019-09-10 17:42:41 +0200

    add body parser

--- app/app.js
+++ app/app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const apiRoutes = require('./api/routes')
+const bodyParser = require('body-parser')
+
 
 module.exports = (()=>{
-
   const app = express()
 
+  // Setup Bodyparser
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
+
+  // Setup API Route
   app.use('/api', apiRoutes)
 
   return app
