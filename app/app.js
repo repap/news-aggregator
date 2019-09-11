2385ddac3f11255be9be4c41918719e9ea2e279f
Dennis Penning <penning.dennis@googlemail.com>
2019-09-11 08:30:17 +0200

    add endpoint logging

--- app/app.js
+++ app/app.js
@@ -3,10 +3,14 @@ require('./db')
 const express = require('express')
 const apiRoutes = require('./api')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 
 module.exports = (()=>{
   const app = express()
+
+  // Setup morgan logger
+  app.use(morgan('combined'))
 
   // Setup Bodyparser
   app.use(bodyParser.urlencoded({ extended: false }))
