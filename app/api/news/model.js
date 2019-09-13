bf29884ee1553af09762cd393ad5ac958a4cede7
Dennis Penning <penning.dennis@googlemail.com>
2019-09-13 08:40:21 +0200

    add created & updated fields add more specific field definitions

--- app/api/news/model.js
+++ app/api/news/model.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const NewsSchema = new Schema({
-  title: String,
-  category: String,
+  title: {type: String, required: true},
+  category: {type: String, required: true},
   subCategory: String,
   url: {
     type: String, 
     unique: true,
-  }
+    required: true,
+  },
+  rate: {type: Number, default: 0},
+  created: {type: Date, default: Date.now, immutable: true},
+  updated: {type: Date, default: Date.now}
 });
 
 const News = mongoose.model('News', NewsSchema)
